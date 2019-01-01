2f1c8b5e32ec5bb9c3f8a5b8c2500e2a8271fe0a
Wendy Calderon <wendycalderon@gmail.com>
2019-01-01 23:32:32 +0000

    Created signup post action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
   get '/signup' do
     erb :"/users/create_user"
   end
+  
+  post '/signup' do 
+    @user = User.new(username: params["username"], email: params["email"], password: params["password"])
+    @user.save
+    session[:id] = @user.id
+    redirect '/tweets'
+  end
 
 end
