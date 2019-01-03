953d2f2b72baed28bfa70adc61e48d79d35135f7
Wendy Calderon <wendycalderon@gmail.com>
2019-01-03 00:09:58 +0000

    Create show action for users to load its tweets

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   has_many :tweets
   validates_presence_of :username, :email, :password
   has_secure_password
+  
+      
+    def slug 
+      username.gsub(" ", "-").downcase
+    end
 end
